2c09e2782e666c3c077d42869799f0b97f813165
Juan David Zea Gonzalez <j.zeag@uniandes.edu.co>
2026-04-28 21:15:27 -0500

    feat: implementacion  punto 1.4 primera parte

---
+++ src/app/user/user-detail/user-detail.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-detail',
+  standalone: false,
+  templateUrl: './user-detail.html',
+})
+export class UserDetail {}
--- src/app/user/user-module.ts
+++ src/app/user/user-module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserList } from './user-list/user-list';
+import { UserDetail } from './user-detail/user-detail';
 
 @NgModule({
-  declarations: [UserList],
+  declarations: [UserList, UserDetail],
   imports: [CommonModule],
-  exports:[UserList]
+  exports: [UserList],
 })
 export class UserModule {}
