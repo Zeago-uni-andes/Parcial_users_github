bd2c73aa580db1a22834df8cfcf5081fe85e40d0
Juan David Zea Gonzalez <j.zeag@uniandes.edu.co>
2026-04-28 20:40:13 -0500

    feat: Creacion primer modulo usuarios

---
+++ src/app/user/user-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserList } from './user-list/user-list';
+
+@NgModule({
+  declarations: [UserList],
+  imports: [CommonModule],
+})
+export class UserModule {}
