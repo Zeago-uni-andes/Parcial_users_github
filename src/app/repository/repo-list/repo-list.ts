f8c250c991c0f050ea0dd6265ab66d68c5daabbb
Juan David Zea Gonzalez <j.zeag@uniandes.edu.co>
2026-04-28 21:35:19 -0500

    feat: Creacion modulo repositorio punto 3.1

---
+++ src/app/repository/repo-list/repo-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repo-list',
+  standalone: false,
+  templateUrl: './repo-list.html',
+  styleUrl: './repo-list.css',
+})
+export class RepoList {}
---
+++ src/app/repository/repository-module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RepoList } from './repo-list/repo-list';
+
+@NgModule({
+  declarations: [RepoList],
+  imports: [CommonModule],
+})
+export class RepositoryModule {}
